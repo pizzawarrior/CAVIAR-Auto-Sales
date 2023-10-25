261c29baa84f8ef3ef7d6ca67af556a5a6d74c05
spiderthatcodes <jessicaldickerson@outlook.com>
2023-10-25 07:46:45 -0700

    button donish

--- ghi/app/src/pages/Inventory.jsx
+++ ghi/app/src/pages/Inventory.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
-import { Wrapper, InventoryContainer } from './style'
+import React from 'react';
+import { Wrapper, InventoryContainer } from './style';
+import Button from '../components/Button/Button';
 
 const Inventory = () => {
-  return (
-    <Wrapper>
-        <InventoryContainer>
-            <div id="one"></div>
-            <div id="two"></div>
-            <div id="three"></div>
-        </InventoryContainer>
-    </Wrapper>
-  )
-}
+    const handleClick = () => console.log('click');
+    return (
+        <Wrapper>
+            <InventoryContainer>
+                <div id='one'>
+                    <Button
+                        text='Test Text'
+                        handleClick={handleClick}
+                    />
+                </div>
+                <div id='two'></div>
+                <div id='three'></div>
+            </InventoryContainer>
+        </Wrapper>
+    );
+};
 
-export default Inventory
+export default Inventory;
