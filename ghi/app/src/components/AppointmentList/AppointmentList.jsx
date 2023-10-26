e8014e267be811176b3396280a8440585f81f172
spiderthatcodes <jessicaldickerson@outlook.com>
2023-10-26 08:14:45 -0700

    commiting to merge

--- ghi/app/src/components/AppointmentList/AppointmentList.jsx
+++ ghi/app/src/components/AppointmentList/AppointmentList.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { ListContainer } from '../ServiceTechs/style'
 
 const AppointmentList = ({ appointments }) => {
+    console.log(appointments)
   return (
     <ListContainer>
         <h1>Service Appointments</h1>
